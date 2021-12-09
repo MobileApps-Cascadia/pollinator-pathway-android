4d8bc49bd44b81b769f34ffa6e54468068c564ea
Andrea Harrison <Andrealharrison@gmail.com>
2021-12-09 14:19:33 -0800

    changed navigation

--- app/src/main/java/edu/cascadia/mobas/pollinatorpathway/ui/login/LoginFragment.java
+++ app/src/main/java/edu/cascadia/mobas/pollinatorpathway/ui/login/LoginFragment.java
@@ -1,4 +1,4 @@
-package edu.cascadia.mobas.pollinatorpathway.ui.notifications;
+package edu.cascadia.mobas.pollinatorpathway.ui.login;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,24 +12,25 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import edu.cascadia.mobas.pollinatorpathway.databinding.FragmentNotificationsBinding;
+import edu.cascadia.mobas.pollinatorpathway.databinding.FragmentLoginBinding;
 
 
-public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
-    private FragmentNotificationsBinding binding;
+public class LoginFragment extends Fragment {
+
+    private LoginViewModel loginViewModel;
+    private FragmentLoginBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        loginViewModel =
+                new ViewModelProvider(this).get(LoginViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        loginViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
