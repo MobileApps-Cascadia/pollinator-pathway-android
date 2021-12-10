fe8c94bfd053874d939a4fb24deadba152add6e1
Andrea Harrison <Andrealharrison@gmail.com>
2021-12-09 17:16:12 -0800

    Updated placeholders for screens

--- app/src/main/java/edu/cascadia/mobas/pollinatorpathway/ui/SetUp/SetUpFragment.java
+++ app/src/main/java/edu/cascadia/mobas/pollinatorpathway/ui/SetUp/SetUpFragment.java
@@ -28,13 +28,7 @@ public class SetUpFragment extends Fragment {
         binding = FragmentSetupBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 
