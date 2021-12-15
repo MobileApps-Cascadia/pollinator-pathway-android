b5874df4330ace9171223e83541c869d94588ce5
Shahin Nezam <shahinnezam@hotmail.com>
2021-12-14 18:33:53 -0800

    Updated Names and Icons

---
+++ app/src/main/java/edu/cascadia/mobas/pollinatorpathway/qrcode.java
@@ -0,0 +1,34 @@
+package edu.cascadia.mobas.pollinatorpathway;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+public class qrcode extends Fragment {
+
+    private QrcodeViewModel mViewModel;
+
+    public static qrcode newInstance() {
+        return new qrcode();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.qrcode_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(QrcodeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
