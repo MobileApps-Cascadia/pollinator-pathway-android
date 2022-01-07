5fbcfe959aa5903c4d3619462ae2a12002524f37
Andrea Harrison <Andrealharrison@gmail.com>
2022-01-07 15:28:48 -0800

    changed name of fragment

--- app/src/main/java/edu/cascadia/mobas/pollinatorpathway/ui/login/profileFragment.java
+++ app/src/main/java/edu/cascadia/mobas/pollinatorpathway/ui/login/profileFragment.java
@@ -24,7 +24,7 @@ public class profileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     @Override
