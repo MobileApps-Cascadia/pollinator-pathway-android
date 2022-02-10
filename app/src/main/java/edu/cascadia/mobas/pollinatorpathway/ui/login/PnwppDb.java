92b950586027e8cac3768615977440ac2e0726ca
FabriceKalvanda <67396845+fabricekalvanda@users.noreply.github.com>
2022-02-10 10:52:48 -0800

    temp commit for update Room database

--- app/src/main/java/edu/cascadia/mobas/pollinatorpathway/ui/login/PnwppDb.java
+++ app/src/main/java/edu/cascadia/mobas/pollinatorpathway/ui/login/PnwppDb.java
@@ -1,10 +1,14 @@
 package edu.cascadia.mobas.pollinatorpathway.ui.login;
 
+import static java.time.chrono.MinguoChronology.INSTANCE;
+
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Profile.class, Planting.class, Box.class}, version = 1)
 public abstract class PnwppDb extends RoomDatabase {
@@ -26,10 +30,30 @@ public abstract class PnwppDb extends RoomDatabase {
     }
 
 
-
     public abstract ProfileDao profileDao();
     public abstract PlantingDao plantingDao();
     public abstract BoxDao boxDao();
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            // If you want to keep data through app restarts,
+            // comment out the following block
+            databaseWriteExecutor.execute(() -> {
+                // Populate the database in the background.
+                // If you want to start with more words, just add them.
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+
+                Word word = new Word("Hello");
+                dao.insert(word);
+                word = new Word("World");
+                dao.insert(word);
+            });
+        }
+    };
 
     private void addStarterData() {
         // Add a few users
