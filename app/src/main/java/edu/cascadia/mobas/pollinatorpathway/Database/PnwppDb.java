75de06f6ad0dd4000df8f603886f317b0f5c625c
Andrea Harrison <Andrealharrison@gmail.com>
2022-03-03 15:49:28 -0800

    added all profiles to database

--- app/src/main/java/edu/cascadia/mobas/pollinatorpathway/Database/PnwppDb.java
+++ app/src/main/java/edu/cascadia/mobas/pollinatorpathway/Database/PnwppDb.java
@@ -67,10 +67,65 @@ public abstract class PnwppDb extends RoomDatabase {
                 p.setEmail("rbcrowder@21acres.org");
                 p.setPhone("326-224-7220");
                 p.setPassword("1234");
-                p.setProfDescription("Organization");
+                p.setType("Organization");
+                p.setProfDescription("The 21 Acres Center for Local Food & Sustainable living is a global leader in sustainable & regenerative practices, serving as a living laboratory & learning center for conscious consumers who want to learn new, more sustainable ways of living.");
                 p.setDateJoined("11/2021");
                 p.setWebsite("https://21acres.org/?s=beevesting");
                 profileDao.insertProfile(p);
+
+                Profile p2 = new Profile();
+                p2.setFirstname("Patricia");
+                p2.setLastname("Newkirk");
+                p2.setOrgName("Songaia Co-Housing Community");
+                p2.setType("Co-Housing");
+                p2.setEmail("patricia.newkirk@gmail.com");
+                p2.setPhone("206.902.8107");
+                p2.setPassword("1234");
+                p2.setDateJoined("11.2021");
+                p2.setWebsite("http://www.songaia.com/the-garden1.html");
+                profileDao.insertProfile(p2);
+
+                Profile p3 = new Profile();
+                p3.setFirstname("Kristi");
+                p3.setLastname("Bolles");
+                p3.setOrgName("Coastal Bank");
+                p3.setEmail("kbolles@coastalbank.com");
+                p3.setPhone("425.482.3103");
+                p3.setPassword("1234");
+                p3.setType("Company");
+                p3.setProfDescription("Coastal Community Bank");
+                p3.setDateJoined("11/2021");
+                p3.setWebsite("kbolles@coastalbank.com");
+                profileDao.insertProfile(p3);
+
+                Profile p4 = new Profile();
+                p4.setFirstname("Alexia");
+                p4.setLastname("Allen");
+                p4.setOrgName("Hawthorn Farm");
+                p4.setEmail("alexiaallen@hotmail.com");
+                p4.setPhone("425.286.5640");
+                p4.setPassword("1234");
+                p4.setType("Farm");
+                p4.setProfDescription("A suburban homestead where happy humans are integrated into the ecological system. We love to teach.");
+                p4.setDateJoined("11/2021");
+                p4.setWebsite("https://www.facebook.com/hawthornfarmheals");
+                profileDao.insertProfile(p4);
+
+                Profile p5 = new Profile();
+                p5.setFirstname("Cameron");
+                p5.setLastname("Graves");
+                p5.setOrgName("Red Barn Farm");
+                p5.setPhone("206.949.3333");
+                p5.setPassword("1234");
+                p5.setType("Farm");
+                p5.setProfDescription("Almost 100 years old and recently restored for the 21st century, The Red Barn Farm is currently raising hens, ducks, goats, honey bees, and harvesting seasonal produce and flowers (green house coming for year-round harvesting!). We’re also a full service venue ready to be host to your next special event, and our small craft whiskey distillery and tasting room is coming soon!");
+                p5.setDateJoined("11/2021");
+                p5.setWebsite("https://redbarnfarm.com/");
+                profileDao.insertProfile(p5);
+
+
+
+
             });
 
             dbExecutor.execute(() -> {
